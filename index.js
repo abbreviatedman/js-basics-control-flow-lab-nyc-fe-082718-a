193d470c28f11ca14ff13e27f2b3cac4b5a8d089
Colin Jaffe <colin.jaffe@gmail.com>
2019-07-10 17:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(rideLength) {
-  if (rideLength <= 400) {
-    return `This one is on me!`;
-  } else if (rideLength > 2000 && rideLength <= 2500) {
-    return 'I will gladly take your thirty bucks.'
-  } else {
-    return 'No can do.';
-  }
-}
+// function scuberGreetingForFeet(rideLength) {
+//   if (rideLength <= 400) {
+//     return `This one is on me!`;
+//   } else if (rideLength > 2000 && rideLength <= 2500) {
+//     return 'I will gladly take your thirty bucks.'
+//   } else {
+//     return 'No can do.';
+//   }
+// }
 
-function ternaryCheckCity(city) {
-  return city === 'NYC'
-    ? 'Ok, sounds good.'
-    : 'No go.'
-}
+// function ternaryCheckCity(city) {
+//   return city === 'NYC'
+//     ? 'Ok, sounds good.'
+//     : 'No go.'
+// }
 
-function switchOnCharmFromTip(tip) {
-  switch (tip) {
-    case 'generous':
-      return 'Thank you so much.';
-    case 'not as generous':
-      return 'Thank you.'
-    default:
-      return 'Bye.'
-  }
-}
+// function switchOnCharmFromTip(tip) {
+//   switch (tip) {
+//     case 'generous':
+//       return 'Thank you so much.';
+//     case 'not as generous':
+//       return 'Thank you.'
+//     default:
+//       return 'Bye.'
+//   }
+// }
+
+
+
+
+// function scuberGreetingForFeet(feet) {
+//   if (feet <= 400) {
+//     return 'This one is on me!';
+//   }
+  
+//   if (feet <= 2000) {
+//     return "That'll be 20 bucks please.";
+//   }
+  
+//   if (feet <= 2500) {
+//     return 'I will gladly take your thirty bucks.';
+//   }
+  
+//   return 'No can do.';
+// }
+
+
+// function ternaryCheckCity(city) {
+//   // if (city === 'NYC') {
+//   //   return 'Ok, sounds good.'
+//   // } else {
+//   //   return "No go."
+//   return ( (city === 'NYC') ? 'Ok, sounds good.' : "No go." );
+//   // }
+// }
+
+// const message = ternaryCheckCity('NYC');
+
+// function switchOnCharmFromTip(tip) {
+//   switch (tip) {
+//     case 'generous':
+//       return 'Thank you so much.'
+//     case 'not as generous':
+//       return 'Thank you.';
+//     default:
+//       return 'Bye.';
+//   }
+// }
+
+
+// Victoria's Version:
+
